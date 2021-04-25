e807db287202e99901f80a2394c0e7017e12c8b6
Marcusvtravers <70954471+Marcusvtravers@users.noreply.github.com>
2021-04-25 13:46:13 +0100

    Completed a draft of CSS
    Finished the CSS draft

--- libs/js/script.js
+++ libs/js/script.js
@@ -10,6 +10,25 @@ $('.fa-plus').click(function(){
     $('#dropdown-small').slideToggle('slow')
 });
 
+$('.fa-angle-down').click(function(){
+    $('.dropdown-add ').slideDown('slow')
+})
+$('.dropdown-add').mouseleave(function(){
+    $('.dropdown-add').slideUp('slow');
+})
+
+
+
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
 
 
--- libs/php/config.php
+++ libs/php/config.php
@@ -1,5 +1,6 @@
 <?php
- /*
+
+
  //for local development
 	$cd_host = "localhost";
 	$cd_port = 3306;
@@ -7,8 +8,8 @@
 	$cd_user = "root"; // user name
 	$cd_password = ""; // password
 	$cd_dbname = "companydirectory"; // database name
-*/
 
+/*
 //live server 
 $cd_host = "localhost";
 $cd_port = 3306;
@@ -16,7 +17,7 @@ $cd_socket = "";
 $cd_user = "marcusvint"; // user name
 $cd_password = "{Iammarcus92}"; // password
 $cd_dbname = "companydirectoryprac"; // database name
-
+*/
 
 	//for live server info // user=marcusvint - pw = {Iammarcus92} // dbname = companydirectoryprac
 ?>
