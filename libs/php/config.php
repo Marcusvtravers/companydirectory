1915e2513545cb1ddf1610d21b7535103f693032
Marcusvtravers <70954471+Marcusvtravers@users.noreply.github.com>
2021-05-01 10:36:06 +0100

    Updated the CSS for companydirectory

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/*
  //for local development
 	$cd_host = "localhost";
 	$cd_port = 3306;
@@ -8,8 +8,8 @@
 	$cd_user = "root"; // user name
 	$cd_password = ""; // password
 	$cd_dbname = "companydirectory"; // database name
+*/
 
-/*
 //live server 
 $cd_host = "localhost";
 $cd_port = 3306;
@@ -17,7 +17,7 @@ $cd_socket = "";
 $cd_user = "marcusvint"; // user name
 $cd_password = "{Iammarcus92}"; // password
 $cd_dbname = "companydirectoryprac"; // database name
-*/
+
 
 	//for live server info // user=marcusvint - pw = {Iammarcus92} // dbname = companydirectoryprac
 ?>
